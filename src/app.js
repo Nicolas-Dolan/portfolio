cb30d15e22b2a335beb5581465052d1d98ef1203
Nicolas-Dolan <nicolas.dolan@ymail.com>
2020-03-28 22:35:06 +0000

    finished adding content

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ class App extends React.Component {
     textOut: 0,
     forwards: true,
     moveable: false,
-    project: 0
+    project: 0,
+    company: 0
   }
 
 
@@ -153,16 +154,46 @@ class App extends React.Component {
       project: param
     })
   }
-
+  stone1Click = () => {
+    this.projects(1)
+  }
+  stone2Click = () => {
+    this.projects(2)
+  }
+  stone3Click = () => {
+    this.projects(3)
+  }
   stone4Click = () => {
     this.projects(4)
   }
 
+  companies(param) {
+    this.setState({ 
+      company: param
+    })
+  }
+
+  company1Click = () => {
+    this.companies(1)
+  }
+  company2Click = () => {
+    this.companies(2)
+  }
+  company3Click = () => {
+    this.companies(3)
+  }
+  company4Click = () => {
+    this.companies(4)
+  }
+  company5Click = () => {
+    this.companies(5)
+  }
+
   render() {
-    const { textIn, textOut, forwards, project } = this.state
+    const { textIn, textOut, forwards, project, company } = this.state
     return (
       <div id="animate-area" className="homePage">
-        <div className="centerIt">
+        <div className="centerIt sideBar">
           <h1 onClick={this.handleClick0}>Nicolas Dolan: Portfolio</h1>
           <div className="navBar">
             <div onClick={this.handleClick1} style={{  textDecoration: textIn === 1 ? 'underline' : '' }}>About</div> | 
@@ -217,9 +248,9 @@ class App extends React.Component {
           <div className={ forwards ? textIn === 3 ? 'showF' : textOut === 3 ? 'hideF' : 'wait' : textIn === 3 ? 'showB' : textOut === 3 ? 'hideB' : 'wait'}>
             <div className="projectsPage contentPage">
               <div className="stoneCircle">
-                <img className="stone stone1" alt="purplestone" src="./../assets/purplestone.png" />
-                <img className="stone stone2" alt="greenstone" src="./../assets/greenstone.png" />
-                <img className="stone stone3" alt="bluestone" src="./../assets/bluestone.png" />
+                <img className="stone stone1" alt="purplestone" src="./../assets/purplestone.png" onClick={this.stone1Click}/>
+                <img className="stone stone2" alt="greenstone" src="./../assets/greenstone.png" onClick={this.stone2Click}/>
+                <img className="stone stone3" alt="bluestone" src="./../assets/bluestone.png" onClick={this.stone3Click}/>
                 <img className="stone stone4" alt="redstone" src="./../assets/redstone.png" onClick={this.stone4Click}/>
               </div>
               <div className="projectDescription">
@@ -229,19 +260,104 @@ class App extends React.Component {
                 </div>
                 <div className={project === 4 ? '' : 'wait'}>
                   <h2>Project 4: Pokémon Unlimited</h2>
-                  <h3>Project: http://bit.ly/pkmn-unlimited ReadMe: http://bit.ly/p4readme</h3>
-                  <p>For my final project, I was allocated 1 week. 
-I decided to work solo because I wanted to have a hand in every part of its development. Paying tribute to a franchise from my childhood, Pokémon, the project used a database to store user-created Pokémon that could then be implemented into a grid-based game. I wanted to create something that was both fun and complex, testing my abilities and drawing on everything I’d learned so far. I created the back-end database using a Python-based framework called Django and managed it with PostgreSQL. I then installed React, a JavaScript-based framework, for the front end and used Axios to make database requests. </p>
-                 
+                  <p>For my final project, I was allocated 1 week. I decided to work solo because I wanted to have a hand in every part of its development. Paying tribute to a franchise from my childhood, Pokémon, the project used a database to store user-created Pokémon that could then be implemented into a grid-based game. I wanted to create something that was both fun and complex, testing my abilities and drawing on everything I’d learned so far. I created the back-end database using a Python-based framework called Django and managed it with PostgreSQL. I then installed React, a JavaScript-based framework, for the front end and used Axios to make database requests. </p>
+                  <a href="http://bit.ly/pkmn-unlimited" target="_blank" rel="noopener noreferrer">See Project</a>
+                  <p></p>
+                  <a href="http://bit.ly/p4readme" target="_blank" rel="noopener noreferrer">See GitHub ReadMe</a>
+                </div>
+                <div className={project === 3 ? '' : 'wait'}>
+                  <h2>Project 3: BeeHive</h2>
+                  <p>I worked in a team of four for a week to create this full-stack application. It was designed to help creatives from different specialties connect and collaborate on projects. We all contributed to the initial idea and design of the website, using wireframes to visualise it. We built the front end using React and Bulma and the back end with MongoDB, Express, and Node.js. I was involved in all parts of the website but took ownership of much of the back end and the messaging system. We used Agile methodology to distribute work, holding ‘scrums’ regularly and tracking our project on Trello.</p>
+                  <a href="http://bit.ly/beehive-app" target="_blank" rel="noopener noreferrer">See Project</a>
+                  <p></p>
+                  <a href="http://bit.ly/p3readme" target="_blank" rel="noopener noreferrer">See GitHub ReadMe</a>
+                </div>
+                <div className={project === 2 ? '' : 'wait'}>
+                  <h2>Project 2: Geography Genius</h2>
+                  <p>This was a quiz about country capitals and flags created after 2 days of pair coding with a course mate. We built it in React and used Axios requests to a third-party API called ‘Rest Countries’ to collect the quiz’s data. I wrote the majority of the logic using JavaScript while my partner focused on the visuals and animations.</p>
+                  <a href="http://bit.ly/geo-genius" target="_blank" rel="noopener noreferrer">See Project</a>
+                  <p></p>
+                  <a href="http://bit.ly/p2readme" target="_blank" rel="noopener noreferrer">See GitHub ReadMe</a>
+                </div>
+                <div className={project === 1 ? '' : 'wait'}>
+                  <h2>Project 1: Goblin Gold</h2>
+                  <p>This was a PacMan-inspired grid-based game, which I built solo in 8 days. I used vanilla JavaScript to implement complex features such as pathfinding and decision trees in addition to spending time using CSS to create a visually appealing experience.</p>
+                  <a href="http://bit.ly/goblin-gold" target="_blank" rel="noopener noreferrer">See Project</a>
+                  <p></p>
+                  <a href="http://bit.ly/p1readme" target="_blank" rel="noopener noreferrer">See GitHub ReadMe</a>              
                 </div>
               </div>
             </div>
           </div>
-          <div className={ forwards ? textIn === 4 ? 'showF' : textOut === 4 ? 'hideF' : 'wait' : textIn === 4 ? 'showB' : textOut === 4 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
-            <p >Experience</p>
+          <div className={ forwards ? textIn === 4 ? 'showF' : textOut === 4 ? 'hideF' : 'wait' : textIn === 4 ? 'showB' : textOut === 4 ? 'hideB' : 'wait'}>
+            <div className="experiencePage contentPage">
+              <h2>Experience & Education</h2>
+              <div className="companies">
+                <div className="dnaContainer"><img className="dna" alt="dna" src="./../assets/dna7.png" /></div>
+                
+                <div className="companyList">
+                  <img className="companyIcon" alt="ga" src="./../assets/ga.png" onClick={this.company1Click}/>
+                  <img className="companyIcon" alt="syneos" src="./../assets/syneos.png" onClick={this.company2Click}/>
+                  <img className="companyIcon" alt="ask" src="./../assets/ask.png" onClick={this.company3Click}/>
+                  <img className="companyIcon" alt="lancet" src="./../assets/lancet.png" onClick={this.company4Click}/>
+                  <img className="companyIcon" alt="kcl" src="./../assets/kcl2.png" onClick={this.company5Click}/>
+                </div>
+                <div className="experienceDesc">
+                  <div className={company === 0 ? '' : 'wait'}>
+                    <p>Click an icon to see details</p>
+                  </div>
+                  <div className={company === 1 ? '' : 'wait'}>
+                    <h3>Software Engineering Immersive</h3>
+                    <h4>General Assembly, London (Dec 2019 – March 2020)</h4>
+                    <p>During this full-time course, I immersed myself in the world of software engineering, exploring a wide range of technologies and development methodologies. Working solo and in teams, I produced four main projects and numerous side projects. The course focused on the MERN stack, but also covered Python with Django and best practices such as test-driven development and the Agile workflow.</p>
+                  </div>
+                  <div className={company === 2 ? '' : 'wait'}>
+                    <h3>Medical Writer</h3>
+                    <h4>Syneos Health, London (July 2019 – Nov 2019)</h4>
+                    <p>Wrote a wide range of scientific content for pharmaceutical and biotechnology companies, such as academic papers and educational materials.</p>
+                    <p>Interpreted scientific data from a variety of sources and adapted this information according to the audience and client brief.</p>
+                    <p>Cross checked, reviewed, and quality checked the work of other employees.</p>
+                    <p>Maintained an in-depth understanding of several therapeutic areas, and actively applied knowledge of the client’s products (pharmaceutical drugs and medical devices), their competitors, and therapy area trends.</p>
+                    <p>Participated in team projects, and adhered to delivery timelines, budgets, and client expectations.</p>
+                  </div>
+                  <div className={company === 3 ? '' : 'wait'}>
+                    <h3>Medical Writer</h3>
+                    <h4>AS&K, London (April 2018 – July 2019)</h4>
+                    <p>Similar role to that at Syneos Health (above).</p>
+                  </div>
+                  <div className={company === 4 ? '' : 'wait'}>
+                    <h3>Assistant Editor & Senior Assistant Editor</h3>
+                    <h4>The Lancet, London (Sept 2014 – March 2018)</h4>
+                    <p>Edited ‘fast-track’ primary research and review material in preparation for publication in the journal, checking scientific accuracy, use of English, clarity, readability, and application of house style.</p>
+                    <p>Managed multiple ongoing projects and met tight deadlines.</p>
+                    <p>Created Excel spreadsheets, which automated time-consuming routine copy editing tasks such as reformatting data tables. I then shared these spreadsheets with my colleagues, saving the company hundreds of hours in labour time.</p>
+                  </div>
+                  <div className={company === 5 ? '' : 'wait'}>
+                    <h3>BSc in Biomedical Science</h3>
+                    <h4>King’s College London (2008 - 2011)</h4>
+                    <p>During this degree, I selected modules mainly related to neuroscience, pharmacology, and physiology. I enjoyed the opportunity to deep-dive into the intricacies of the human body and found that the more I learned, the more my curiosity grew. I’ve maintained this interest throughout my career and, although I am not exclusively looking for health-care-related opportunities, I am very interested in continuing to work in the medical/health sector as a developer.</p>
+                  </div>
+                </div>
+              </div>
+              
+              
+            </div>
           </div>
-          <div className={ forwards ? textIn === 5 ? 'showF' : textOut === 5 ? 'hideF' : 'wait' : textIn === 5 ? 'showB' : textOut === 5 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
-            <p >Contact</p>
+          <div className={ forwards ? textIn === 5 ? 'showF' : textOut === 5 ? 'hideF' : 'wait' : textIn === 5 ? 'showB' : textOut === 5 ? 'hideB' : 'wait'}>
+            <div className="contactPage contentPage">
+              <h2>Contact</h2>
+              <div className="contactDetails">
+                <div>
+                  <p>Email: nicolas.dolan@ymail.com</p>
+                  <p>GitHub: github.com/Nicolas-Dolan</p>
+                  <p>LinkedIn: linkedin.com/in/nicolas-dolan/</p>
+                </div>
+                <div>
+                  <p>Location:</p>
+                  <img className="earth" alt="earth" src="./../assets/earth2.png" />
+                </div>
+              </div>
+            </div>
           </div>
           
         </div>
