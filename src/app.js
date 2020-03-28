58b4c5c644c05e91670d8e8cd439406ed1ab1c81
Nicolas-Dolan <nicolas.dolan@ymail.com>
2020-03-28 17:14:05 +0000

    added more content to the portfolio

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ class App extends React.Component {
     textIn: 0,
     textOut: 0,
     forwards: true,
-    moveable: false
+    moveable: false,
+    project: 0
   }
 
 
@@ -147,8 +148,18 @@ class App extends React.Component {
     this.inOut(5)
   }
 
+  projects(param) {
+    this.setState({ 
+      project: param
+    })
+  }
+
+  stone4Click = () => {
+    this.projects(4)
+  }
+
   render() {
-    const { textIn, textOut, forwards } = this.state
+    const { textIn, textOut, forwards, project } = this.state
     return (
       <div id="animate-area" className="homePage">
         <div className="centerIt">
@@ -179,14 +190,52 @@ class App extends React.Component {
             </div>
             
           </div>
-          <div className={ forwards ? textIn === 1 ? 'showF' : textOut === 1 ? 'hideF' : 'wait' : textIn === 1 ? 'showB' : textOut === 1 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
-            <p >About me</p>
+          <div className={ forwards ? textIn === 1 ? 'showF' : textOut === 1 ? 'hideF' : 'wait' : textIn === 1 ? 'showB' : textOut === 1 ? 'hideB' : 'wait'}>
+            <div className="aboutPage contentPage">
+              <h2>About</h2>
+              <p>I am an enthusiastic and dedicated professional who is transitioning to a career in software engineering following a successful career as a medical writer and editor.  </p>
+
+              <p>I have recently completed a 3-month software engineering immersive at General Assembly where I learned technical skills in full-stack web development. Additionally, I can leverage excellent problem-solving and attention-to-detail skills developed while writing and analysing complicated scientific texts in my previous roles. </p>
+
+              <p>I work well on my own initiative and as part of a team, and have experience managing multiple ongoing projects while working both in-house and for clients. I maintain a broad interest in science and technology, and I am keen to continue contributing in these areas through my work.</p>
+            </div>
           </div>
-          <div className={ forwards ? textIn === 2 ? 'showF' : textOut === 2 ? 'hideF' : 'wait' : textIn === 2 ? 'showB' : textOut === 2 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
-            <p >Skills</p>
+          <div className={ forwards ? textIn === 2 ? 'showF' : textOut === 2 ? 'hideF' : 'wait' : textIn === 2 ? 'showB' : textOut === 2 ? 'hideB' : 'wait'}>
+            <div className="skillsPage contentPage">
+              <h2>Skills</h2>
+              <h3>Technologies</h3>
+              <h4>Front End </h4>
+              <p>React, JavaScript, HTML, CSS, Sass, Bulma, AJAX</p>
+              <h4>Back End</h4>
+              <p>Node.js, Express.js, MongoDB, Python, Django, PostgreSQL, SQL, Insomnia, TablePlus, Robo 3T</p>
+              <h3>Auxiliary</h3>
+              <p>Heroku, Git, GitHub, Command line, Mocha, Chai, Trello, npm, Yarn, Pip, Computer science fundamentals</p>
+              <h3>Soft Skills</h3>
+              <p>Problem solving, Attention to detail, Team work, Test-driven development, Wireframing, Initiative, Communication, Agile development, Time management, Analysis</p>
+            </div>
           </div>
-          <div className={ forwards ? textIn === 3 ? 'showF' : textOut === 3 ? 'hideF' : 'wait' : textIn === 3 ? 'showB' : textOut === 3 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
-            <p >Projects</p>
+          <div className={ forwards ? textIn === 3 ? 'showF' : textOut === 3 ? 'hideF' : 'wait' : textIn === 3 ? 'showB' : textOut === 3 ? 'hideB' : 'wait'}>
+            <div className="projectsPage contentPage">
+              <div className="stoneCircle">
+                <img className="stone stone1" alt="purplestone" src="./../assets/purplestone.png" />
+                <img className="stone stone2" alt="greenstone" src="./../assets/greenstone.png" />
+                <img className="stone stone3" alt="bluestone" src="./../assets/bluestone.png" />
+                <img className="stone stone4" alt="redstone" src="./../assets/redstone.png" onClick={this.stone4Click}/>
+              </div>
+              <div className="projectDescription">
+                <div className={project === 0 ? '' : 'wait'}>
+                  <h2>Projects</h2>
+                  <p>Click a stone to view a project</p>
+                </div>
+                <div className={project === 4 ? '' : 'wait'}>
+                  <h2>Project 4: Pokémon Unlimited</h2>
+                  <h3>Project: http://bit.ly/pkmn-unlimited ReadMe: http://bit.ly/p4readme</h3>
+                  <p>For my final project, I was allocated 1 week. 
+I decided to work solo because I wanted to have a hand in every part of its development. Paying tribute to a franchise from my childhood, Pokémon, the project used a database to store user-created Pokémon that could then be implemented into a grid-based game. I wanted to create something that was both fun and complex, testing my abilities and drawing on everything I’d learned so far. I created the back-end database using a Python-based framework called Django and managed it with PostgreSQL. I then installed React, a JavaScript-based framework, for the front end and used Axios to make database requests. </p>
+                 
+                </div>
+              </div>
+            </div>
           </div>
           <div className={ forwards ? textIn === 4 ? 'showF' : textOut === 4 ? 'hideF' : 'wait' : textIn === 4 ? 'showB' : textOut === 4 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
             <p >Experience</p>
