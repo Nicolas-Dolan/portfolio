de7cc8f4bd3109b184d53f67ea944eb58ef304b1
Nicolas-Dolan <nicolas.dolan@ymail.com>
2020-03-28 13:46:07 +0000

    added title page

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     textIn: 0,
     textOut: 0,
     forwards: true,
-    moveable: true
+    moveable: false
   }
 
 
@@ -18,6 +18,12 @@ class App extends React.Component {
 
   componentDidMount() {
     window.onscroll = () => this.handleScroll()
+    setTimeout(() => {
+      this.setState({
+        moveable: true
+      })
+    }, 1500)
+
     // setInterval(() => {
     //   this.setState({
     //     moveable: true
@@ -67,7 +73,7 @@ class App extends React.Component {
 
   moveForwards = () => {
     const textInP = this.state.textIn
-    if (textInP === 2) {
+    if (textInP === 5) {
       this.setState({ 
         textOut: this.state.textIn,
         textIn: 0,
@@ -88,7 +94,7 @@ class App extends React.Component {
     if (textInP === 0) {
       this.setState({ 
         textOut: this.state.textIn,
-        textIn: 2,
+        textIn: 5,
         forwards: false
       })
     } else {
@@ -121,6 +127,9 @@ class App extends React.Component {
     // }
   }
 
+  handleClick0  = () => {
+    this.inOut(0)
+  }
 
   handleClick1  = () => {
     this.inOut(1)
@@ -128,26 +137,62 @@ class App extends React.Component {
   handleClick2  = () => {
     this.inOut(2)
   }
+  handleClick3  = () => {
+    this.inOut(3)
+  }
+  handleClick4  = () => {
+    this.inOut(4)
+  }
+  handleClick5  = () => {
+    this.inOut(5)
+  }
 
   render() {
     const { textIn, textOut, forwards } = this.state
     return (
       <div id="animate-area" className="homePage">
         <div className="centerIt">
-          <h1>Nicolas Dolan: Portfolio</h1>
-          <button onClick={this.handleClick1}>Text 1</button>
-          <button onClick={this.handleClick2}>Text 2</button>
-          <button onClick={this.moveForwards}>Forwards</button>
-          <button onClick={this.moveBackwards}>Backwards</button>
+          <h1 onClick={this.handleClick0}>Nicolas Dolan: Portfolio</h1>
+          <div className="navBar">
+            <div onClick={this.handleClick1} style={{  textDecoration: textIn === 1 ? 'underline' : '' }}>About</div> | 
+            <div onClick={this.handleClick2} style={{  textDecoration: textIn === 2 ? 'underline' : '' }}>Skills</div> | 
+            <div onClick={this.handleClick3} style={{  textDecoration: textIn === 3 ? 'underline' : '' }}>Projects</div> | 
+            <div onClick={this.handleClick4} style={{  textDecoration: textIn === 4 ? 'underline' : '' }}>Experience</div> | 
+            <div onClick={this.handleClick5} style={{  textDecoration: textIn === 5 ? 'underline' : '' }}>Contact</div>
+          </div>
+          <div className="buttonBar">
+            <div onClick={this.moveBackwards}><img className="arrow" alt="left arrow" src="./../assets/left-arrow.png"/></div>
+            <div onClick={this.moveForwards}><img className="arrow" alt="right arrow" src="./../assets/right-arrow.png"/></div>
+            
+          </div>
+          
         </div>
         <div className="content">
-          {/* <div className={ textIn === 1 ? 'showF' : textOut === 1 ? 'hideF' : 'wait' } style={{ backgroundColor: 'gray' }}> */}
-          <div className={ forwards ? textIn === 1 ? 'showF' : textOut === 1 ? 'hideF' : 'wait' : textIn === 1 ? 'showB' : textOut === 1 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
-            <p >This is test text 1</p>
+          <div className={ forwards ? textIn === 0 ? 'showF' : textOut === 0 ? 'hideF' : 'wait' : textIn === 0 ? 'showB' : textOut === 0 ? 'hideB' : 'wait'}>
+            <div className="titlePage contentPage">
+              <h1>Full Stack Web Developer</h1>
+              <div onClick={this.moveForwards}>
+                <h2>Scroll</h2>
+                <img className="scrollArrow"  alt="down arrow" src="./../assets/down-arrow.png"/>
+              </div>
+              
+            </div>
+            
           </div>
-          {/* <div className={ textIn === 2 ? 'showF' : textOut === 2 ? 'hideF' : 'wait' } style={{ backgroundColor: 'gray' }}> */}
+          <div className={ forwards ? textIn === 1 ? 'showF' : textOut === 1 ? 'hideF' : 'wait' : textIn === 1 ? 'showB' : textOut === 1 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
+            <p >About me</p>
+          </div>
           <div className={ forwards ? textIn === 2 ? 'showF' : textOut === 2 ? 'hideF' : 'wait' : textIn === 2 ? 'showB' : textOut === 2 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
-            <p >This is test text 2</p>
+            <p >Skills</p>
+          </div>
+          <div className={ forwards ? textIn === 3 ? 'showF' : textOut === 3 ? 'hideF' : 'wait' : textIn === 3 ? 'showB' : textOut === 3 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
+            <p >Projects</p>
+          </div>
+          <div className={ forwards ? textIn === 4 ? 'showF' : textOut === 4 ? 'hideF' : 'wait' : textIn === 4 ? 'showB' : textOut === 4 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
+            <p >Experience</p>
+          </div>
+          <div className={ forwards ? textIn === 5 ? 'showF' : textOut === 5 ? 'hideF' : 'wait' : textIn === 5 ? 'showB' : textOut === 5 ? 'hideB' : 'wait'} style={{ backgroundColor: 'gray' }}>
+            <p >Contact</p>
           </div>
           
         </div>
